71a19e27622f5893da5f28d79c1455059dfacdef
jagapatr <jagapatr@motorola.com>
2021-04-09 17:33:48 -0300

    Finished Game

---
+++ PTC_Game-Velha/Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace PTC_Game_Velha
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bt1 = new System.Windows.Forms.Button();
+            this.bt2 = new System.Windows.Forms.Button();
+            this.bt3 = new System.Windows.Forms.Button();
+            this.bt6 = new System.Windows.Forms.Button();
+            this.bt5 = new System.Windows.Forms.Button();
+            this.bt4 = new System.Windows.Forms.Button();
+            this.bt9 = new System.Windows.Forms.Button();
+            this.bt8 = new System.Windows.Forms.Button();
+            this.bt7 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btRef = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // bt1
+            // 
+            this.bt1.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt1.Location = new System.Drawing.Point(12, 12);
+            this.bt1.Name = "bt1";
+            this.bt1.Size = new System.Drawing.Size(80, 80);
+            this.bt1.TabIndex = 0;
+            this.bt1.UseVisualStyleBackColor = true;
+            this.bt1.Click += new System.EventHandler(this.bt1_Click);
+            // 
+            // bt2
+            // 
+            this.bt2.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt2.Location = new System.Drawing.Point(98, 12);
+            this.bt2.Name = "bt2";
+            this.bt2.Size = new System.Drawing.Size(80, 80);
+            this.bt2.TabIndex = 1;
+            this.bt2.UseVisualStyleBackColor = true;
+            this.bt2.Click += new System.EventHandler(this.bt2_Click);
+            // 
+            // bt3
+            // 
+            this.bt3.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt3.Location = new System.Drawing.Point(184, 12);
+            this.bt3.Name = "bt3";
+            this.bt3.Size = new System.Drawing.Size(80, 80);
+            this.bt3.TabIndex = 2;
+            this.bt3.UseVisualStyleBackColor = true;
+            this.bt3.Click += new System.EventHandler(this.bt3_Click);
+            // 
+            // bt6
+            // 
+            this.bt6.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt6.Location = new System.Drawing.Point(184, 98);
+            this.bt6.Name = "bt6";
+            this.bt6.Size = new System.Drawing.Size(80, 80);
+            this.bt6.TabIndex = 5;
+            this.bt6.UseVisualStyleBackColor = true;
+            this.bt6.Click += new System.EventHandler(this.bt6_Click);
+            // 
+            // bt5
+            // 
+            this.bt5.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt5.Location = new System.Drawing.Point(98, 98);
+            this.bt5.Name = "bt5";
+            this.bt5.Size = new System.Drawing.Size(80, 80);
+            this.bt5.TabIndex = 4;
+            this.bt5.UseVisualStyleBackColor = true;
+            this.bt5.Click += new System.EventHandler(this.bt5_Click);
+            // 
+            // bt4
+            // 
+            this.bt4.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt4.Location = new System.Drawing.Point(12, 98);
+            this.bt4.Name = "bt4";
+            this.bt4.Size = new System.Drawing.Size(80, 80);
+            this.bt4.TabIndex = 3;
+            this.bt4.UseVisualStyleBackColor = true;
+            this.bt4.Click += new System.EventHandler(this.bt4_Click);
+            // 
+            // bt9
+            // 
+            this.bt9.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt9.Location = new System.Drawing.Point(184, 184);
+            this.bt9.Name = "bt9";
+            this.bt9.Size = new System.Drawing.Size(80, 80);
+            this.bt9.TabIndex = 8;
+            this.bt9.UseVisualStyleBackColor = true;
+            this.bt9.Click += new System.EventHandler(this.bt9_Click);
+            // 
+            // bt8
+            // 
+            this.bt8.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt8.Location = new System.Drawing.Point(98, 184);
+            this.bt8.Name = "bt8";
+            this.bt8.Size = new System.Drawing.Size(80, 80);
+            this.bt8.TabIndex = 7;
+            this.bt8.UseVisualStyleBackColor = true;
+            this.bt8.Click += new System.EventHandler(this.bt8_Click);
+            // 
+            // bt7
+            // 
+            this.bt7.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt7.Location = new System.Drawing.Point(12, 184);
+            this.bt7.Name = "bt7";
+            this.bt7.Size = new System.Drawing.Size(80, 80);
+            this.bt7.TabIndex = 6;
+            this.bt7.UseVisualStyleBackColor = true;
+            this.bt7.Click += new System.EventHandler(this.bt7_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 277);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(138, 16);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "JOGADOR ATUAL:";
+            // 
+            // btRef
+            // 
+            this.btRef.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btRef.Location = new System.Drawing.Point(2, 316);
+            this.btRef.Name = "btRef";
+            this.btRef.Size = new System.Drawing.Size(90, 27);
+            this.btRef.TabIndex = 10;
+            this.btRef.Text = "REFRESH";
+            this.btRef.UseVisualStyleBackColor = true;
+            this.btRef.Click += new System.EventHandler(this.btRef_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(189, 318);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "SAIR";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.ClientSize = new System.Drawing.Size(276, 346);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btRef);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.bt9);
+            this.Controls.Add(this.bt8);
+            this.Controls.Add(this.bt7);
+            this.Controls.Add(this.bt6);
+            this.Controls.Add(this.bt5);
+            this.Controls.Add(this.bt4);
+            this.Controls.Add(this.bt3);
+            this.Controls.Add(this.bt2);
+            this.Controls.Add(this.bt1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "   Jogo do DiDi";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bt1;
+        private System.Windows.Forms.Button bt2;
+        private System.Windows.Forms.Button bt3;
+        private System.Windows.Forms.Button bt6;
+        private System.Windows.Forms.Button bt5;
+        private System.Windows.Forms.Button bt4;
+        private System.Windows.Forms.Button bt9;
+        private System.Windows.Forms.Button bt8;
+        private System.Windows.Forms.Button bt7;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btRef;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ PTC_Game-Velha/Form1.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PTC_Game_Velha
+{
+    public partial class Form1 : Form
+    {
+
+        public string jogadorAtual = "X";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void zerar()
+        {
+            bt1.Text = "";
+            bt2.Text = "";
+            bt3.Text = "";
+            bt4.Text = "";
+            bt5.Text = "";
+            bt6.Text = "";
+            bt7.Text = "";
+            bt8.Text = "";
+            bt9.Text = "";
+            jogadorAtual = "X";
+
+        }
+        public void ganhar()
+        {
+            MessageBox.Show("!!! O CANALHA -> " + jogadorAtual + " <- GANHOU!!!!");
+            zerar();
+        }
+
+        public string InverteJogador()
+        {
+            if (jogadorAtual == "X")
+            {
+                return "O";
+            }
+            else
+            {
+                return "X";
+            }
+        }
+        public bool Verificar(string jogador)
+        {
+            if (bt1.Text == jogador && bt2.Text == jogador && bt3.Text == jogador)
+            {
+                ganhar();
+                return true;
+            }
+            if (bt1.Text == jogador && bt4.Text == jogador && bt7.Text == jogador)
+            {
+                ganhar();
+                return true;
+            }
+            if (bt1.Text == jogador && bt5.Text == jogador && bt9.Text == jogador)
+            {
+                ganhar();
+                return true;
+            }
+            if (bt3.Text == jogador && bt5.Text == jogador && bt7.Text == jogador)
+            {
+                ganhar();
+                return true;
+            }
+            if (bt3.Text == jogador && bt6.Text == jogador && bt9.Text == jogador)
+            {
+                ganhar();
+                return true;
+            }
+            if (bt4.Text == jogador && bt5.Text == jogador && bt6.Text == jogador)
+            {
+                ganhar();
+                return true;
+            }
+            if (bt7.Text == jogador && bt8.Text == jogador && bt9.Text == jogador)
+            {
+                ganhar();
+                return true;
+            }
+            if (bt2.Text == jogador && bt5.Text == jogador && bt8.Text == jogador)
+            {
+                ganhar();
+                return true;
+            }
+
+            label1.Text = "JOGADOR ATUAL: " + jogadorAtual; 
+            jogadorAtual = InverteJogador();
+
+            return false;
+
+        }
+
+        private void btRef_Click(object sender, EventArgs e)
+        {
+            zerar();
+        }
+
+        private void bt1_Click(object sender, EventArgs e)
+        {
+            if (bt1.Text == "")
+            {
+                bt1.Text = jogadorAtual;
+                Verificar(jogadorAtual);
+            }
+        }
+
+        private void bt2_Click(object sender, EventArgs e)
+        {
+            if (bt2.Text == "")
+            {
+                bt2.Text = jogadorAtual;
+                Verificar(jogadorAtual);
+            }
+        }
+
+        private void bt3_Click(object sender, EventArgs e)
+        {
+            if (bt3.Text == "")
+            {
+                bt3.Text = jogadorAtual;
+                Verificar(jogadorAtual);
+            }
+        }
+
+        private void bt4_Click(object sender, EventArgs e)
+        {
+            if (bt4.Text == "")
+            {
+                bt4.Text = jogadorAtual;
+                Verificar(jogadorAtual);
+            }
+        }
+
+        private void bt5_Click(object sender, EventArgs e)
+        {
+            if (bt5.Text == "")
+            {
+                bt5.Text = jogadorAtual;
+                Verificar(jogadorAtual);
+            }
+        }
+
+        private void bt6_Click(object sender, EventArgs e)
+        {
+            if (bt6.Text == "")
+            {
+                bt6.Text = jogadorAtual;
+                Verificar(jogadorAtual);
+            }
+        }
+
+        private void bt7_Click(object sender, EventArgs e)
+        {
+            if (bt7.Text == "")
+            {
+                bt7.Text = jogadorAtual;
+                Verificar(jogadorAtual);
+            }
+        }
+
+        private void bt8_Click(object sender, EventArgs e)
+        {
+            if (bt8.Text == "")
+            {
+                bt8.Text = jogadorAtual;
+                Verificar(jogadorAtual);
+            }
+        }
+
+        private void bt9_Click(object sender, EventArgs e)
+        {
+            if (bt9.Text == "")
+            {
+                bt9.Text = jogadorAtual;
+                Verificar(jogadorAtual);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+    }
+}
